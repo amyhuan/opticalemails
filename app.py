880a55846e3514083b989359bb7a9811cbd3dcfe
Amy Huang <amyhuan@microsoft.com>
2023-11-21 15:19:03 -0800

    apis with hard coded timestamps

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ from bs4 import BeautifulSoup
 from azure.storage.blob import BlobServiceClient
 from azure.data.tables import TableServiceClient
 from openai import AzureOpenAI
+from flask_socketio import SocketIO, send, emit
 
 dotenv.load_dotenv()
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 client = AzureOpenAI(
     azure_endpoint="https://amyhuan-openai.openai.azure.com",
@@ -42,29 +44,61 @@ def query_table_by_timestamp(connection_string, table_name, start_time, end_time
 
     return list(entities)
 
-@app.route('/emailids', methods=['GET'])
-def get_email_ids():
-    start_timestamp = request.args.get('startTime', default='').replace('\'', '')
-    end_timestamp = request.args.get('endTime', default='').replace('\'', '')
-
-    table_name = "maintenances"
-    start_time = "2023-11-10T00:00:00Z"
-    end_time = "2023-11-15T00:00:00Z"
-    entities = query_table_by_timestamp(STORAGE_CONNECTION_STRING, table_name, start_time, end_time)
-    row_keys = extract_row_keys(entities)
+def email_ids_by_time_range(start_time, end_time):
+    entities = query_table_by_timestamp(STORAGE_CONNECTION_STRING, "maintenances", start_time, end_time)
+    row_keys = [entity['RowKey'] for entity in entities]
 
     print(f"Email IDs: {row_keys}")
     return row_keys
 
+def get_blob_client(connection_string, container_name, blob_name):
+    """
+    Returns a blob client for the specified blob in Azure Blob Storage.
+
+    :param connection_string: The connection string to the Azure Storage account.
+    :param container_name: The name of the container in which the blob resides.
+    :param blob_name: The name of the blob to access.
+    :return: BlobClient for the specified blob.
+    """
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    return blob_client
+
+def parse_html_blob(blob_client):
+    """
+    Parses an HTML blob and returns the text of each tag.
+    """
+    # Download the blob content
+    blob_data = blob_client.download_blob().readall()
+    html_content = blob_data.decode('utf-8')
+
+    # Parse the HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Extract text from specific tags (e.g., body)
+    body_text = soup.body.get_text(strip=True) if soup.body else None
+
+    return body_text
+
+@app.route('/emailids', methods=['GET'])
+def get_email_ids():
+    # start_timestamp = request.args.get('startTime', default='').replace('\'', '')
+    # end_timestamp = request.args.get('endTime', default='').replace('\'', '')
+    start_time = "2023-11-20T20:02:26+00:00"
+    end_time = "2023-11-22T20:02:26+00:00"
+    return email_ids_by_time_range(start_time, end_time)
+
 @app.route('/emailsummaries', methods=['GET'])
 def get_email_summaries():
-    ids = [] # TODO: get ids with api call
+    start_time = "2023-11-20T20:02:26+00:00"
+    end_time = "2023-11-22T20:02:26+00:00"
+
+    ids = email_ids_by_time_range(start_time, end_time)
     summaries = []
     for id in ids:
         try:
             blob_client = get_blob_client(STORAGE_CONNECTION_STRING, 'emails', id)
             text = parse_html_blob(blob_client)
-            emit('message', f"Email text: \n {text}")
 
             res = client.chat.completions.create(
                 model="vscode-gpt",
@@ -76,7 +110,7 @@ def get_email_summaries():
                 max_tokens=256
             )
             summaries.append(res.choices[0].message.content)
-        except e:
+        except Exception as e:
             print(e)
             continue
     return summaries
@@ -86,4 +120,4 @@ def base_path():
     return jsonify("success")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True, port=8080, host="0.0.0.0")
